cc119bf51ea92cbc14ee8c5512ac5c1c7632a44a
5225225 <5225225@users.noreply.github.com>
2016-04-28 17:04:44 +0100

    Ignore site packages from coverage check.

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 flake8 --max-line-length=99 *.py
 
-coverage run test.py ; coverage report -m --fail-under 100 --include="*.py"
+coverage run --omit='*/site-packages/*' test.py ; coverage report -m --fail-under 100 --include="*.py"
